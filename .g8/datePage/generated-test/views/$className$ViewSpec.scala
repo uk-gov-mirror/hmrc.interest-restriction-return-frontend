041b7820be18d0f14c1d17a1fc159bf82fc5b284
Ladon Jackson <31283663+LadonJackson@users.noreply.github.com>
2019-11-01 12:34:05 +0000

    [CIR-86]: update stringPage g8 template

--- .g8/datePage/generated-test/views/$className$ViewSpec.scala
+++ .g8/datePage/generated-test/views/$className$ViewSpec.scala
@@ -22,7 +22,7 @@ class $className$ViewSpec extends QuestionViewBehaviours[LocalDate] {
     val view = application.injector.instanceOf[$className$View]
 
     def applyView(form: Form[_]): HtmlFormat.Appendable =
-      view.apply(form, NormalMode)(request, messages)
+      view.apply(form, NormalMode)(fakeRequest, messages)
 
     behave like normalPage(applyView(form), messageKeyPrefix)
 
