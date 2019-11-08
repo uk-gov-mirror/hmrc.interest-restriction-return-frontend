8f368fa40538230201b10f43dd4d228d6a7aa6ec
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2019-11-08 10:40:21 +0000

    [g8stuff] Correct Scaffolds

--- .g8/yesNoPage/migrations/$className__snake$.sh
+++ .g8/yesNoPage/migrations/$className__snake$.sh
@@ -50,14 +50,7 @@ echo "Adding helper method to CheckYourAnswersHelper"
 awk '/class/ {\
      print;\
      print "";\
-     print "  def $className;format="decap"$: Option[AnswerRow] = userAnswers.get($className$Page) map {";\
-     print "    x =>";\
-     print "      AnswerRow(";\
-     print "        HtmlFormat.escape(messages(\"$className;format="decap"$.checkYourAnswersLabel\")),";\
-     print "        yesOrNo(x),";\
-     print "        routes.$className$Controller.onPageLoad(CheckMode).url";\
-     print "      )"
-     print "  }";\
+     print "  def $className;format="decap"$: Option[SummaryListRow] = yesOrNo($className$Page, routes.$className$Controller.onPageLoad(CheckMode))";\
      next }1' ../app/utils/CheckYourAnswersHelper.scala > tmp && mv tmp ../app/utils/CheckYourAnswersHelper.scala
 
 echo "Migration $className;format="snake"$ completed"
