61af494ed382dc1a7adabf62769d536b0e1641d7
“mywyau” <47248104+mywyau@users.noreply.github.com>
2020-02-21 09:26:48 +0000

    CIR-527 updated IT tests

--- it/controllers/ukCompanies/NetTaxInterestIncomeOrExpenseControllerISpec.scala
+++ it/controllers/ukCompanies/NetTaxInterestIncomeOrExpenseControllerISpec.scala
@@ -19,6 +19,7 @@ package controllers.ukCompanies
 import assets.UkCompanyITConstants._
 import assets.{BaseITConstants, PageTitles}
 import models.NetTaxInterestIncomeOrExpense.NetTaxInterestExpense
+import models.NetTaxInterestIncomeOrExpense.NetTaxInterestExpense
 import pages.ukCompanies.UkCompaniesPage
 import play.api.http.Status._
 import play.api.libs.json.Json
