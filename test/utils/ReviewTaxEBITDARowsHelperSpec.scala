38c83b12d3c80f4b71746db0c227f324b0110b72
Ladon Jackson <31283663+LadonJackson@users.noreply.github.com>
2020-03-03 09:19:01 +0000

    [ebitda-checkmode]: added checkmode to ebitda derived value

--- test/utils/ReviewTaxEBITDARowsHelperSpec.scala
+++ test/utils/ReviewTaxEBITDARowsHelperSpec.scala
@@ -19,7 +19,7 @@ package utils
 import assets.constants.fullReturn.UkCompanyConstants._
 import assets.messages.BaseMessages
 import base.SpecBase
-import models.NormalMode
+import models.{CheckMode, NormalMode}
 import pages.ukCompanies.UkCompaniesPage
 import viewmodels.SummaryListRowHelper
 
@@ -43,17 +43,17 @@ class ReviewTaxEBITDARowsHelperSpec extends SpecBase with SummaryListRowHelper w
           summaryListRow(
             ukCompanyModelMax.companyDetails.companyName,
             currencyFormat(ukCompanyModelMax.taxEBITDA.getOrElse(0)),
-            controllers.ukCompanies.routes.EnterCompanyTaxEBITDAController.onPageLoad(1, NormalMode) -> BaseMessages.changeLink
+            controllers.ukCompanies.routes.EnterCompanyTaxEBITDAController.onPageLoad(1, CheckMode) -> BaseMessages.changeLink
           ),
           summaryListRow(
             ukCompanyModelMax.companyDetails.companyName,
             currencyFormat(ukCompanyModelMax.taxEBITDA.getOrElse(0)),
-            controllers.ukCompanies.routes.EnterCompanyTaxEBITDAController.onPageLoad(2, NormalMode) -> BaseMessages.changeLink
+            controllers.ukCompanies.routes.EnterCompanyTaxEBITDAController.onPageLoad(2, CheckMode) -> BaseMessages.changeLink
           ),
           summaryListRow(
             ukCompanyModelMax.companyDetails.companyName,
             currencyFormat(ukCompanyModelMax.taxEBITDA.getOrElse(0)),
-            controllers.ukCompanies.routes.EnterCompanyTaxEBITDAController.onPageLoad(3, NormalMode) -> BaseMessages.changeLink
+            controllers.ukCompanies.routes.EnterCompanyTaxEBITDAController.onPageLoad(3, CheckMode) -> BaseMessages.changeLink
           )
         )
       }
