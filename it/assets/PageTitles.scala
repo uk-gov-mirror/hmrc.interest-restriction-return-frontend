7ce4c1d2ef67127adb7e7df8670e60e17564f634
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2020-03-12 11:45:46 +0000

    [CIR-626] Added IT Tests

--- it/assets/PageTitles.scala
+++ it/assets/PageTitles.scala
@@ -2,6 +2,7 @@ package assets
 
 object PageTitles {
   val reactivationAmount = "Add a reactivation amount"
+  val reviewReactivations = "Review reactivations for companies in the group"
   val accountingPeriodStart = "When does the worldwide period of account start?"
   val accountingPeriodEnd = "When does the worldwide period of account end?"
   val reviewAndComplete = "Interest Restriction Return"
